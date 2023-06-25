acf04c56e0e94bbc45a33ed74509c7664fbee7c1
Risbel Suarez <risbel961019@gmail.com>
2023-06-24 23:54:02 -0400

    feat: Add Car Catalogue section

--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,12 @@ import Hero from "./Hero";
 import Button from "./Button";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import Filter from "./Filter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
 export {
-	Hero, Button, Navbar, Footer
+	Hero, Button, Navbar, Footer, SearchBar, Filter, SearchManufacturer, CarCard, CarDetails
 }
